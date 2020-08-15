fae7b4bf3c516bfbcd590c97482fa600f5fe097a
unknown <mingyu514@naver.com>
2020-08-16 08:56:05 +0900

    before designed

--- main.js
+++ main.js
@@ -27,29 +27,61 @@ window.onload = ()=>{
         hy.style.filter = 'brightness(100%)';
     })
 
+    // 기존 css에서 플로팅 배너 위치(top)값을 가져와 저장한다.
+	var floatPosition = parseInt($("#fixed-banner").css('top'));
 
-    // curriculum button
-    var cir1 = document.querySelector('#cir1');
-    var cir2 = document.querySelector('#cir2');
-    var speechBubble3 = document.querySelector('#speech-bubble3');
-    var speechBubble4 = document.querySelector('#speech-bubble4');
+	$(window).scroll(function() {
+		// 현재 스크롤 위치를 가져온다.
+		var scrollTop = $(window).scrollTop();
+		var newPosition = scrollTop + floatPosition + "px";
 
-    cir1.addEventListener('click',()=>{
-        var open3 = speechBubble3.style.display;
-        if(open3 !== 'none'){
-            speechBubble3.style.display = 'none';
-        }else{
-            speechBubble3.style.display = 'inline';
-            speechBubble4.style.display = 'none';
-        }
-    });
-    cir2.addEventListener('click',()=>{
-        var open4 = speechBubble4.style.display;
-        if(open4 === 'inline'){
-            speechBubble4.style.display = 'none';
-        }else{
-            speechBubble4.style.display = 'inline';
-            speechBubble3.style.display = 'none';
-        }
-    });
+
+		$("#fixed-banner").stop().animate({
+			"top" : newPosition
+		}, 300);
+
+    }).scroll();
+    
+    // accordion
+    function toggleIcon(e) {
+        $(e.target)
+          .prev(".panel-heading")
+          .find(".more-less")
+          .toggleClass("glyphicon-plus glyphicon-minus");
+      }
+      $(".panel-group").on("hidden.bs.collapse", toggleIcon);
+      $(".panel-group").on("shown.bs.collapse", toggleIcon);
+
+    // tab(0~4개)
+    var tabnum;
+    var tablist = [
+      ['공지사항',`공지사항입력 ㄱㄱ`],
+      ['신청하기','신청 ㄱㄱ'],
+      ['환불규정','환불은 ㄴㄴ'],
+      ['강의 진행','강의 ㄱㄱ'],
+      ['1:1 상담','상담 ㄱㄱ'],
+    ];
+
+    var faq1 = document.querySelector('#faq1');
+    var faq2 = document.querySelector('#faq2');
+    var faq3 = document.querySelector('#faq3');
+    var faq4 = document.querySelector('#faq4');
+    var faq5 = document.querySelector('#faq5');
+    var faqtitle = document.querySelector('#faqtitle');
+    var faqtext = document.querySelector('#faqtext');
+
+    document.querySelectorAll('.faqtab').forEach(function(e){
+        e.addEventListener('click', function(){
+            var tabtxt = this.textContent;
+            for(tabnum=0;tabnum<5;tabnum++){
+                if(tabtxt === tablist[tabnum][0]){
+                    break;
+                }
+            }
+            console.log(`이번클릭은 ${tabnum}`);
+            faqtitle.textContent = tabtxt;
+            faqtext.innerHTML = tablist[tabnum][1];
+            console.log(`title: ${faqtitle}`);
+        })
+    })
 }
